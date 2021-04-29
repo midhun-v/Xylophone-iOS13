e4f0cb275f07c33ac1e4b8d8e13ba55683d958f4
midhun-v <narenmids@gmail.com>
2021-04-29 12:31:19 +0530

    Implemented sound

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
     
+    var bombSoundEffect: AVAudioPlayer?
+    let soundArray = ["C.wav","D.wav","E.wav","F.wav","G.wav","A.wav","B.wav"]
 
+    func playSound(sound: String) {
+        let path = Bundle.main.path(forResource: sound, ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        do {
+            bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+            bombSoundEffect?.play()
+        } catch {
+            print ("couldn't load file")
+        }
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        switch sender.currentTitle! {
+        
+        case "C":
+            playSound(sound: "C.wav")
+        case "D":
+            playSound(sound: "D.wav")
+        case "E":
+            playSound(sound: "E.wav")
+        case "F":
+            playSound(sound: "F.wav")
+        case "G":
+            playSound(sound: "G.wav")
+        case "A":
+            playSound(sound: "A.wav")
+        case "B":
+            playSound(sound: "B.wav")
+        default:
+            print("Some other character")
+        }
+    }
+    
 }
 
